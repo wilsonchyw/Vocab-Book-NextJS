7de4c5f1632dfb1023c6291fdfb5d6b70de340f9
wilsonych <0913hung@gmail.com>
2022-02-27 09:28:51 +0000

    Fix token error 2

--- lib/localToken.ts
+++ lib/localToken.ts
@@ -1,8 +1,8 @@
 export default function getLocalToken(): string | null {
     if (typeof window !== "undefined") {
-        const token = JSON.parse(localStorage.getItem("token") || "")
+        const token = JSON.parse(localStorage.getItem("token") || "null")
         if (!token) return null
-        if (token.timeStamp < Date.now() - 3600000){
+        if (token.timeStamp < Date.now() - 3600000) {
             localStorage.removeItem("token")
             return null
         }
